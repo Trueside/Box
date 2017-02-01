ccfa680f310b163f114e91c3f4c19a1e9cf449f1
Trueside <afftarpunk@mail.ru>
2017-02-01 22:49:32 +0300

    Full name

--- src/com/company/Boxer.java
+++ src/com/company/Boxer.java
@@ -9,27 +9,27 @@ public class Boxer extends Human {
     public int blockSkill;
     public int rang = 11;
 
-/*      public Boxer(){
-        Systems.printing("Say your firstname!");
-        this.firstName = Systems.scaning();
-        Systems.printing("Say your lastname!");
-        this.lastName = Systems.scaning();
-        Systems.printing("Say your nickname!");
-        this.nickname = Systems.scaning();
-        Systems.printing("Say your country!");
-        this.country = Systems.scaning();
-        Systems.printing("Your hand position is... !");
-        this.handsPosition = Systems.scaning();
-        Systems.printing("Your power is...");
-        this.power = Integer.parseInt(Systems.scaning());
-        Systems.printing("Your health is...");
-        this.health = Integer.parseInt(Systems.scaning());
-        Systems.printing("Your stamina is...");
-        this.stamina = Integer.parseInt(Systems.scaning());
-        Systems.printing("Your block skill is...");
-        this.blockSkill = Integer.parseInt(Systems.scaning());
-        Systems.printing("Well done, son!");
-    }*/
+//      public Boxer(){
+//        Systems.printing("Say your firstname!");
+//        this.firstName = Systems.scaning();
+//        Systems.printing("Say your lastname!");
+//        this.lastName = Systems.scaning();
+//        Systems.printing("Say your nickname!");
+//        this.nickname = Systems.scaning();
+//        Systems.printing("Say your country!");
+//        this.country = Systems.scaning();
+//        Systems.printing("Your hand position is... !");
+//        this.handsPosition = Systems.scaning();
+//        Systems.printing("Your power is...");
+//        this.power = Integer.parseInt(Systems.scaning());
+//        Systems.printing("Your health is...");
+//        this.health = Integer.parseInt(Systems.scaning());
+//        Systems.printing("Your stamina is...");
+//        this.stamina = Integer.parseInt(Systems.scaning());
+//        Systems.printing("Your block skill is...");
+//        this.blockSkill = Integer.parseInt(Systems.scaning());
+//        Systems.printing("Well done, son!");
+//    }
 
     public Boxer(String nickname, String handsPosition, int power, int health, int stamina, int blockSkill) {
         this.nickname = nickname;
